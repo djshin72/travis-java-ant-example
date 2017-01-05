7069cc5b1cb52bada9b1f4c52cef229c40089b72
KR005717 <KR005717@9.194.111.2>
2017-01-05 15:46:46 +0900

    init Travis CI testing simple Java.

---
+++ TravisCIExample/src/com/travis/java/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.travis.java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World, I'm using Travis-CI!");
+	}
+}
